51978007682b37b87ddd33ad9163fdbd20b62355
Navian <primeguyy@gmail.com>
2021-04-28 19:10:26 +0000

    app ui updated with more styles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,12 +3,6 @@ import ProductsPage from "./ProductsPage";
 const Home = () => {
   return (
     <>
-      <h3>
-        Hello from the world of Redux and all the reducers you can think of...
-      </h3>
-
-      <button className="btn">Change State</button>
-
       <ProductsPage />
     </>
   );
